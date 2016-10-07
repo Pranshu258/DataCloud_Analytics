b252bd40d2a4ea150e1d80602ef9631b66b142a3
Pranshu Gupta <pranshug258@outlook.com>
2016-10-07 21:27:20 +0530

    added logout functionality from dashboard

--- datacloud/dashboard/views.py
+++ datacloud/dashboard/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-
+from django.contrib.auth import logout
 
 def index(request):
     if request.user.is_authenticated:
-        return HttpResponse("<h1>Hello, I am your Dashboard !!</h1>")
+        return HttpResponse("<h1>Hello, I am your Dashboard !!</h1><a href='logout/'>logout</a>")
     else:
         return HttpResponse("<h1>You are not logged in !!!</h1>")
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
