f36829a5890ebcb64d767e847f516a84d6d66aee
Pranshu Gupta <pranshug258@outlook.com>
2016-10-26 22:32:35 +0530

    get requests done, post failing because of csrf

--- datacloud/datacloud/urls.py
+++ datacloud/datacloud/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.UserFormView.as_view(), name='register'),
     url(r'^login/$', views.LoginFormView.as_view(), name='login'),
     url(r'^admin/', admin.site.urls),
-    url(r'^dashboard/', include('dashboard.urls'))
+    url(r'^dashboard/', include('dashboard.urls')),
+    url(r'^services/', include('services.urls'))
 ]
--- datacloud/services/views.py
+++ datacloud/services/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
 # Create your views here.
+def api(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            rclient = request.GET.get('client')
+            rparam = request.GET.get('params')
+            return HttpResponse(models.Activity.objects.filter(client=rclient).values_list(rparam, flat=True))
+        else:
+            return HttpResponse("you are not logged in")
+    if request.method == "POST":
+        client = request.POST["client"]
+        activity = request.POST["activity"]
+        activity_type = request.POST["activity_type"]
+        user = request.POST["user"]
+        age = request.POST["age"]
+        gender = request.POST["gender"]
+        location = request.POST["location"]
+        return HttpResponse(client + " " + location)
