0683910c328944f484d27dd52888dace6e70d32f
Pranshu Gupta <pranshug258@outlook.com>
2016-10-26 12:38:32 +0530

    implemented the activity model, now implement the API

--- datacloud/services/models.py
+++ datacloud/services/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 class Activity(models.Model):
+	timestamp = models.DateTimeField(default=timezone.now) 
 	client = models.CharField(max_length=255)
 	activity = models.CharField(max_length=255)
 	activity_type = models.CharField(max_length=255)
@@ -9,3 +11,7 @@ class Activity(models.Model):
 	GENDERS = (('M', 'Male'), ('F', 'Female'), ('O', 'Other'))
 	gender = models.CharField(max_length=1, choices=GENDERS)
 	location = models.CharField(max_length=255)
+
+	def __str__(self):
+		text = '\nClient: ' + self.client + '\n' + 'Activity: ' + self.activity + '\n' + 'Type: ' + self.activity_type + '\n' + 'Age: ' + str(self.age) + '\n' + 'Gender: ' + self.gender + '\n' + 'Timestamp: ' + str(self.timestamp) + '\n'
+		return text
