ec1bc870e2fd4a5206ed2afb85377ae8d7bbbe05
Pranshu Gupta <pranshug258@outlook.com>
2016-10-27 03:50:59 +0530

    implemented data collection using get request for an image from the client

--- datacloud/services/urls.py
+++ datacloud/services/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'api/$', views.api, name='api'),
+    url(r'api$', views.api, name='api'),
+    url(r'collect$', views.collect, name='collect')
 ]
--- datacloud/services/views.py
+++ datacloud/services/views.py
@@ -4,19 +4,43 @@ from . import models
 
 # Create your views here.
 def api(request):
-    if request.method == "GET":
-        if request.user.is_authenticated:
-            rclient = request.GET.get('client')
-            rparam = request.GET.get('params')
-            return HttpResponse(models.Activity.objects.filter(client=rclient).values_list(rparam, flat=True))
-        else:
-            return HttpResponse("you are not logged in")
-    if request.method == "POST":
-        client = request.POST["client"]
-        activity = request.POST["activity"]
-        activity_type = request.POST["activity_type"]
-        user = request.POST["user"]
-        age = request.POST["age"]
-        gender = request.POST["gender"]
-        location = request.POST["location"]
-        return HttpResponse(client + " " + location)
+    if request.user.is_authenticated:
+        rclient = request.GET.get('client')
+        rparam = request.GET.get('params')
+        return HttpResponse(models.Activity.objects.filter(client=rclient).values_list(rparam, flat=True))
+    else:
+        return HttpResponse("you are not logged in")
+
+def collect(request):
+    # add assymetric key encryption here to protect the data that was sent
+    
+    # Following parameters are recieved via the get request
+    rclient = request.GET.get("client")
+    ractivity = request.GET.get("activity")
+    ractivity_type = request.GET.get("activity_type")
+    ruser = request.GET.get("user")
+    rage = request.GET.get("age")
+    rgender = request.GET.get("gender")
+    rlocation = request.GET.get("location")
+    # http://127.0.0.1:8000/services/collect?client=netflix&activity=thekilling&activity_type=thriller&user=pikachu&age=21&gender=M&location=india
+
+    # Validate the data recieved
+    validated = False
+    if rclient and ractivity and ractivity_type and ruser and rage and rgender and rlocation:
+        validated = True
+
+    # Create an entry in the activity table
+    if validated:
+        activity = models.Activity(
+            client = rclient,
+            activity = ractivity,
+            activity_type = ractivity_type,
+            user = ruser,
+            age = rage,
+            gender = rgender,
+            location = rlocation
+        )
+        activity.save()
+
+    return HttpResponse("recieved")
+
